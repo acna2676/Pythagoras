002c783e13943fe11bcc86334deddab8b0432abb
ryosuke nakaishi <acna2676@gmail.com>
2022-10-10 19:01:13 +0900

    note: add filter message

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import calendar
 import datetime
-import json
 import os
 
 import boto3
@@ -9,28 +7,21 @@ from boto3.dynamodb.conditions import Key
 from chalice import Chalice, Response
 from dateutil.relativedelta import relativedelta
 
-from chalicelib import API_KEY
-
 app = Chalice(app_name='pythagoras')
 
 
 def get_database():
     endpoint = os.environ.get('DB_ENDPOINT')
     if endpoint:
-        print("db endpoint enabled: ", endpoint)
         return boto3.resource('dynamodb', endpoint_url=endpoint)
     else:
-        print("db endpoint disabled")
         return boto3.resource('dynamodb')
 
 
 class DBAccessor:
 
-    # NOTE ファクトリにできそう
     dynamodb = get_database()
     TABLE_NAME = os.environ.get('DB_TABLE_NAME')
-    # FIXME table name:  pythagorasになる
-    print("table name: ", TABLE_NAME)
     table = dynamodb.Table(TABLE_NAME)
 
     def __init__(self, pk):
@@ -51,76 +42,75 @@ class DBAccessor:
         return items
 
 
+@app.route('/favicon.ico')
+def get_favicon():
+    return Response(body="", status_code=301, headers={'Location': 'https://pythagoras-dev-content.s3.ap-northeast-1.amazonaws.com/favicon.ico', "Content-Type": "image/x-icon", "Access-Control-Allow-Origin": "*"})
+
+
+@app.route('/chalicelib/static/css/style.css')
+def get_css():
+    with open('chalicelib/static/css/style.css') as f:
+        data = f.read()
+    return Response(body=data, status_code=200, headers={"Content-Type": "text/css", "Access-Control-Allow-Origin": "*"})
+
+
 @app.route('/', methods=["GET"], content_types=["*/*"])
-def index():
+def display_index_page():
     stocks_fileter = 0
     query_params = app.current_request.query_params
     if query_params:
         stocks_fileter = int(query_params.get('stocks'))
-    print("query = ", query_params)
-
-    access_token = API_KEY
-    headers = {'Authorization': 'Bearer '+access_token}
-
-    selected_articles = []
 
     dt_now = datetime.datetime.now()
     dt_prev_year = str((dt_now - relativedelta(months=1)).year)
     dt_prev_month = str((dt_now - relativedelta(months=1)).month)
+    dt_year = str((dt_now - relativedelta(months=0)).year)
+    dt_month = str((dt_now - relativedelta(months=0)).month)
     dt_next_year = str((dt_now + relativedelta(months=1)).year)
     dt_next_month = str((dt_now + relativedelta(months=1)).month)
 
     target_year = str(dt_now.year)  # '2021'
     target_month = str(dt_now.month).zfill(2)  # '03'
-    print(target_year, target_month)
 
     pk = target_year + '-' + target_month
 
     db_accessor = DBAccessor(pk)
     selected_articles_sorted = db_accessor.get_items()
     # stock数がquery_params以上のものでフィルタリング
-    print("selected_articles_sorted = ", selected_articles_sorted)
     selected_articles_sorted = filter(lambda x: x["stocks"] >= stocks_fileter, selected_articles_sorted)
 
     context = {"selected_articles": selected_articles_sorted, "dt_prev_year": dt_prev_year,
-               "dt_prev_month": dt_prev_month.zfill(2), "dt_next_year": dt_next_year, "dt_next_month": dt_next_month.zfill(2)}
+               "dt_prev_month": dt_prev_month.zfill(2), "dt_year": dt_year, "dt_month": dt_month.zfill(2), "dt_next_year": dt_next_year, "dt_next_month": dt_next_month.zfill(2)}
     template = render("chalicelib/templates/index.html", context)
     return Response(template, status_code=200, headers={"Content-Type": "text/html;charset=UTF-8", "Access-Control-Allow-Origin": "*"})
 
 
-@app.route('/{date}', methods=["GET"], content_types=["*/*"])  # /<date>とすると/の場合にもfavicon.icoで実行されてしまうためcreated-atを挟んでいる
-def other(date):
-    if date == 'favicon.ico':
-        # return index()
-        return
-    else:
-        stocks_fileter = 0
-        query_params = app.current_request.query_params
-        if query_params:
-            stocks_fileter = int(query_params.get('stocks'))
-        print("query = ", query_params)
+@app.route('/{date}', methods=["GET"], content_types=["*/*"])
+def display_page_for_target_month(date):
+    stocks_fileter = 0
+    query_params = app.current_request.query_params
+    if query_params:
+        stocks_fileter = int(query_params.get('stocks'))
 
-        access_token = API_KEY
-        headers = {'Authorization': 'Bearer '+access_token}
+    target_year = date[:4]  # '2021'
+    target_month = date[4:]  # '03'
+    dt_now = datetime.date(int(target_year), int(target_month), 1)
+    dt_prev_year = str((dt_now - relativedelta(months=1)).year)
+    dt_prev_month = str((dt_now - relativedelta(months=1)).month)
+    dt_year = str((dt_now - relativedelta(months=0)).year)
+    dt_month = str((dt_now - relativedelta(months=0)).month)
+    dt_next_year = str((dt_now + relativedelta(months=1)).year)
+    dt_next_month = str((dt_now + relativedelta(months=1)).month)
 
-        selected_articles = []
-        target_year = date[:4]  # '2021'
-        target_month = date[4:]  # '03'
-        dt_now = datetime.date(int(target_year), int(target_month), 1)
-        dt_prev_year = str((dt_now - relativedelta(months=1)).year)
-        dt_prev_month = str((dt_now - relativedelta(months=1)).month)
-        dt_next_year = str((dt_now + relativedelta(months=1)).year)
-        dt_next_month = str((dt_now + relativedelta(months=1)).month)
+    pk = target_year + '-' + target_month
+    db_accessor = DBAccessor(pk)
+    selected_articles_sorted = db_accessor.get_items()
+    selected_articles_sorted = filter(lambda x: x["stocks"] >= stocks_fileter, selected_articles_sorted)
 
-        pk = target_year + '-' + target_month
-        db_accessor = DBAccessor(pk)
-        selected_articles_sorted = db_accessor.get_items()
-        selected_articles_sorted = filter(lambda x: x["stocks"] >= stocks_fileter, selected_articles_sorted)
-
-        context = {"selected_articles": selected_articles_sorted, "dt_prev_year": dt_prev_year,
-                   "dt_prev_month": dt_prev_month.zfill(2), "dt_next_year": dt_next_year, "dt_next_month": dt_next_month.zfill(2)}
-        template = render("chalicelib/templates/index.html", context)
-        return Response(template, status_code=200, headers={"Content-Type": "text/html; charset=UTF-8", "Access-Control-Allow-Origin": "*"})
+    context = {"selected_articles": selected_articles_sorted, "dt_prev_year": dt_prev_year,
+               "dt_prev_month": dt_prev_month.zfill(2), "dt_year": dt_year, "dt_month": dt_month.zfill(2), "dt_next_year": dt_next_year, "dt_next_month": dt_next_month.zfill(2)}
+    template = render("chalicelib/templates/index.html", context)
+    return Response(template, status_code=200, headers={"Content-Type": "text/html; charset=UTF-8", "Access-Control-Allow-Origin": "*"})
 
 
 def render(tpl_path, context):
@@ -128,22 +118,5 @@ def render(tpl_path, context):
     return jinja2.Environment(loader=jinja2.FileSystemLoader(path or "./")).get_template(filename).render(context)
 
 
-@app.route('/chalicelib/static/css/style.css')
-def cssindex():
-    with open('chalicelib/static/css/style.css') as f:
-        data = f.read()
-    return Response(body=data, status_code=200, headers={"Content-Type": "text/css", "Access-Control-Allow-Origin": "*"})
-
-
-@app.route('/chalicelib/favicon.ico')
-def faviconindex():
-    with open('chalicelib/static/favicon.ico', 'rb') as fp:
-        data = fp.read()
-    return Response(body=data, status_code=200, headers={"Content-Type": "image/png", "Access-Control-Allow-Origin": "*"})
-
-
 if __name__ == "__main__":
     app.run(debug=True, port=8888, threaded=True)
-
-# 実行方法
-# chalice local
--- stack/s3/create_stack.sh
+++ stack/s3/create_stack.sh
@@ -1,2 +1,3 @@
 aws cloudformation create-stack --stack-name  pythagoras-dev-sam-package --template-body file://sam_package_bucket.yml
+aws cloudformation create-stack --stack-name  pythagoras-dev-content-bucket --template-body file://content_bucket.yml
 
