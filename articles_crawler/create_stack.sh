00fb6114417e8cf615a5fec79d7550e9d808b7ad
nakaishi ryosuke <acna2676@gmail.com>
2024-05-19 07:18:15 +0900

    change cron

--- articles_crawler/create_stack.sh
+++ articles_crawler/create_stack.sh
@@ -1,5 +1,5 @@
 sam package --template-file template.yml --output-template-file template-packaged.yml --s3-bucket pythagoras-dev-sam-package
-./set_env_variable.sh
+source ./set_env_variables_aws.sh
 sam deploy --template-file template-packaged.yml --stack-name pythagoras-dev-articlesCrawler --capabilities CAPABILITY_IAM --parameter-overrides ApiKey=$API_KEY UrlQiitaApiV2=$URL_QIITA_API_V2
 
 aws cloudformation delete-stack --stack-name pythagoras-dev-articlesCrawler
